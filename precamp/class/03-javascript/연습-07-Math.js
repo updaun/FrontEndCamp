7a076f0b3faba3969355e463383f3a8e50debf48
updaun <updauney@daum.net>
2024-02-26 23:21:52 +0900

    feat: Math, DOM

---
+++ precamp/class/03-javascript/연습-07-Math.js
@@ -0,0 +1,12 @@
+Math.floor(Math.random() * 1000000)
+// 629172
+String(Math.floor(Math.random() * 1000000))
+// '329218'
+String(Math.floor(Math.random() * 1000000)).padStart(6,"0")
+// '278018'
+let result = String(Math.floor(Math.random() * 1000000)).padStart(6,"0")
+// undefined
+result
+// '063016'
+result
+// '063016'
