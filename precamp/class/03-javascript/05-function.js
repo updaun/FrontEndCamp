99f1332f55901155ace8be1989592d14e5eb1238
updaun <updauney@daum.net>
2024-03-02 23:26:11 +0900

    feat: 내장함수 리뷰
    - setInterval clearInterval

--- precamp/class/03-javascript/05-function.js
+++ precamp/class/03-javascript/05-function.js
@@ -1,16 +1,35 @@
+let isStarted = false
+
+
+
 const number_send = () => {
-    const token = String(Math.floor(Math.random() * 1000000)).padStart(6, '0')
-    document.getElementById('target').innerText = token
-    document.getElementById('target').style.color = "#" + token
-    let time = 180
-    setInterval(function() {
-        if (time >= 0) {
-            let min = Math.floor(time / 60)
-            let sec = String((time%60)).padStart(2, "0")
-            document.getElementById('timer').innerText = min + ":" + sec
-            time = time -1
-        } else {
-            document.getElementById('finish').disabled = true
-        }
-    }, 1000)
+
+    if(isStarted === false) {
+        // 타이머가 작동중이 아닐 때
+        isStarted = true
+        document.getElementById('finish').disabled = false
+        const token = String(Math.floor(Math.random() * 1000000)).padStart(6, '0')
+        document.getElementById('target').innerText = token
+        document.getElementById('target').style.color = "#" + token
+
+        let time = 10
+        let timer
+
+        timer = setInterval(function() {
+            if (time >= 0) {
+                let min = Math.floor(time / 60)
+                let sec = String((time%60)).padStart(2, "0")
+                document.getElementById('timer').innerText = min + ":" + sec
+                time = time -1
+            } else {
+                document.getElementById('finish').disabled = true
+                isStarted = false
+                clearInterval(timer)
+            }
+        }, 1000)
+
+    } else {
+        // 타이머가 작동중일 때
+    }
+
 }
