e3b4073beb63ea95df5e68e401bd0b6eb3535185
updaun <updauney@daum.net>
2024-03-05 23:44:15 +0900

    feat: final 과제 - 디자인 위주 작업

---
+++ precamp/class/final.js
@@ -0,0 +1,13 @@
+const changeFocus = () => {
+    let phone1 = document.getElementById('phone1').value
+    if (phone1.length === 3) {
+        document.getElementById('phone2').focus()
+    }
+}
+
+const changeFocus2 = () => {
+    let phone2 = document.getElementById('phone2').value
+    if (phone2.length === 4) {
+        document.getElementById('phone3').focus()
+    }
+}
