213a7f3a4204711cc14075591e25137f2e79531b
updaun <updauney@daum.net>
2024-03-31 22:52:48 +0900

    feat: js d-day counter logic

---
+++ js-basic/D-Day-counter/script.js
@@ -0,0 +1,50 @@
+const messageContainer = document.querySelector("#d-day-message");
+const container = document.querySelector("#d-day-container");
+
+// container.style.display = "none";
+messageContainer.innerHTML = "<h3>D-Day를 입력해 주세요.</h3>";
+
+const dateFormMaker = function () {
+  let inputYear = document.querySelector("#target-year-input").value;
+  let inputMonth = document.querySelector("#target-month-input").value;
+  let inputDate = document.querySelector("#target-date-input").value;
+
+  const dateFormat = `${inputYear}-${inputMonth}-${inputDate}`;
+
+  return dateFormat;
+};
+
+const counterMaker = function () {
+  const targetDateInput = dateFormMaker();
+  const nowDate = new Date();
+  const targetDate = new Date(targetDateInput).setHours(0, 0, 0, 0);
+  const remaining = (targetDate - nowDate) / 1000;
+  if (remaining === 0 || remaining < 0) {
+    console.log("타이머가 종료되었습니다.");
+    messageContainer.innerHTML = "<h3>타이머가 종료되었습니다.</h3>";
+    return;
+  } else if (isNaN(remaining)) {
+    console.log("유효한 시간대가 아닙니다.");
+    messageContainer.innerHTML = "<h3>유효한 시간대가 아닙니다.</h3>";
+    return;
+  }
+
+  const remainingObj = {
+    remainingDate: Math.floor(remaining / 3600 / 24),
+    remainingHour: Math.floor((remaining / 3600) % 24),
+    remainingMinute: Math.floor((remaining / 60) % 60),
+    remainingSecond: Math.floor(remaining % 60),
+  };
+
+  const documentObj = {
+    days: document.getElementById("days"),
+    hours: document.getElementById("hours"),
+    minutes: document.getElementById("minutes"),
+    seconds: document.getElementById("seconds"),
+  };
+
+  documentObj.days.textContent = remainingObj.remainingDate;
+  documentObj.hours.textContent = remainingObj.remainingHour;
+  documentObj.minutes.textContent = remainingObj.remainingMinute;
+  documentObj.seconds.textContent = remainingObj.remainingSecond;
+};
