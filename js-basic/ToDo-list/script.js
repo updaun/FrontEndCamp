622a705c8abe69707dcf1e0988b1e29af98b414b
updaun <updauney@daum.net>
2024-04-07 03:24:12 +0900

    feat: js todo list logic update
    - addEventListener
    - scope
    - localStorage save Object to using JSON

--- js-basic/ToDo-list/script.js
+++ js-basic/ToDo-list/script.js
@@ -1,13 +1,82 @@
-const inputInput = document.querySelector("#todo-input");
+const todoInput = document.querySelector("#todo-input");
+const todoList = document.querySelector("#todo-list");
+
+const savedTodoList = JSON.parse(localStorage.getItem("saved-items"));
+
+const createTodo = function (storageData) {
+  let todoContents = todoInput.value;
+  if (storageData) {
+    todoContents = storageData.contents;
+  }
+  const newLi = document.createElement("li");
+  const newSpan = document.createElement("span");
+  const newBtn = document.createElement("button");
+
+  newSpan.addEventListener("click", () => {
+    newLi.classList.toggle("complete");
+    saveItemsFn();
+  });
+  newBtn.addEventListener("click", () => {
+    newLi.classList.toggle("complete");
+    saveItemsFn();
+  });
+  newLi.addEventListener("dblclick", () => {
+    newLi.remove();
+    saveItemsFn();
+  });
+
+  if (storageData?.complete) {
+    newLi.classList.add("complete");
+  }
+
+  newSpan.textContent = todoContents;
+  newLi.appendChild(newBtn);
+  newLi.appendChild(newSpan);
+  todoList.appendChild(newLi);
+  todoInput.value = "";
+  saveItemsFn();
+};
 
 const keyCodeCheck = function () {
-  if (window.event.keyCode === 13 && inputInput.value !== "") {
-    const todoList = document.querySelector("#todo-list");
-    const newLi = document.createElement("li");
-    const newSpan = document.createElement("span");
-    newSpan.textContent = inputInput.value;
-    newLi.appendChild(newSpan);
-    todoList.appendChild(newLi);
-    inputInput.value = "";
+  if (window.event.keyCode === 13 && todoInput.value !== "") {
+    createTodo();
   }
 };
+
+const deleteAll = function () {
+  const liList = document.querySelectorAll("li");
+  for (let li of liList) {
+    li.remove();
+  }
+  saveItemsFn();
+};
+
+const saveItemsFn = function () {
+  const saveItems = [];
+
+  for (let i = 0; i < todoList.children.length; i++) {
+    const todo = todoList.children[i].querySelector("span").textContent;
+    const todoObj = {
+      contents: todo,
+      complete: todoList.children[i].classList.contains("complete"),
+    };
+    saveItems.push(todoObj);
+  }
+
+  //   if (saveItems.length === 0) {
+  //     localStorage.removeItem("saved-items");
+  //   } else {
+  //     localStorage.setItem("saved-items", JSON.stringify(saveItems));
+  //   }
+
+  // 삼항 연사자로 변경
+  saveItems.length === 0
+    ? localStorage.removeItem("saved-items")
+    : localStorage.setItem("saved-items", JSON.stringify(saveItems));
+};
+
+if (savedTodoList) {
+  for (let i = 0; i < savedTodoList.length; i++) {
+    createTodo(savedTodoList[i]);
+  }
+}
