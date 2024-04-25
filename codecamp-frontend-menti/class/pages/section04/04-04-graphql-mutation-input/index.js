1eee8796421bfccfb2f9752707e48cefced1a375
updaun <updauney@daum.net>
2024-04-25 20:51:46 +0900

    feat: 04-04-graphql-mutation-input

---
+++ codecamp-frontend-menti/class/pages/section04/04-04-graphql-mutation-input/index.js
@@ -0,0 +1,53 @@
+import { useMutation, gql } from "@apollo/client";
+import { useState } from "react";
+
+const 나의그래프큐엘셋팅 = gql`
+  mutation createBoard($writer: String, $title: String, $contents: String) {
+    createBoard(writer: $writer, title: $title, contents: $contents) {
+      _id
+      number
+      message
+    }
+  }
+`;
+
+export default function GraphqlMutationInputPage() {
+  const [writer, setWriter] = useState("");
+  const [title, setTitle] = useState("");
+  const [contents, setContents] = useState("");
+
+  const [나의함수] = useMutation(나의그래프큐엘셋팅);
+
+  const onClickSubmit = async () => {
+    const result = await 나의함수({
+      // variables 이게 $ 역할을 함
+      variables: {
+        writer: writer,
+        title: title,
+        contents: contents,
+      },
+    });
+    console.log(result);
+  };
+
+  const onChangeWriter = (event) => {
+    setWriter(event.target.value);
+  };
+
+  const onChangeTitle = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const onChangeContents = (event) => {
+    setContents(event.target.value);
+  };
+
+  return (
+    <div>
+      작성자: <input type="text" onChange={onChangeWriter} />
+      제목: <input type="text" onChange={onChangeTitle} />
+      내용: <input type="text" onChange={onChangeContents} />
+      <button onClick={onClickSubmit}>GRAPHQL-API(비동기) 요청하기</button>
+    </div>
+  );
+}
