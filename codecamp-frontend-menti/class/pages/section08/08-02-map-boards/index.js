4e6f9a6b42823dbcac3f7aa7f8ab20eac25f26ba
updaun <updauney@daum.net>
2024-05-08 22:09:07 +0900

    feat: 08-02-map-boards

---
+++ codecamp-frontend-menti/class/pages/section08/08-02-map-boards/index.js
@@ -0,0 +1,33 @@
+import { useQuery, gql } from "@apollo/client";
+
+const FETCH_BOARDS = gql`
+  query {
+    fetchBoards {
+      number
+      writer
+      title
+      contents
+    }
+  }
+`;
+
+export default function StaticRoutingMovedPage() {
+  const { data } = useQuery(FETCH_BOARDS);
+
+  console.log(data?.fetchBoards);
+
+  return (
+    <div>
+      {data?.fetchBoards.map((el) => (
+        <div>
+          <span>
+            <input type="checkbox" />
+          </span>
+          <span style={{ margin: "10px" }}>{el.number}</span>
+          <span style={{ margin: "10px" }}>{el.title}</span>
+          <span style={{ margin: "10px" }}>{el.writer}</span>
+        </div>
+      ))}
+    </div>
+  );
+}
