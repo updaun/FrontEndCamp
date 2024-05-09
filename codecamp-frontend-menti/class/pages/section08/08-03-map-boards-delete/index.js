5069300bf9014ea1cc31bc4c3cd3a21e3e0c49cb
updaun <updauney@daum.net>
2024-05-09 18:47:28 +0900

    feat: 08-03-map-boards-delete
    - map 활용 시 key 부여
    - fragment 사용

---
+++ codecamp-frontend-menti/class/pages/section08/08-03-map-boards-delete/index.js
@@ -0,0 +1,55 @@
+import { useQuery, gql, useMutation } from "@apollo/client";
+
+const FETCH_BOARDS = gql`
+  query {
+    fetchBoards {
+      number
+      writer
+      title
+      contents
+    }
+  }
+`;
+
+const DELETE_BOARD = gql`
+  mutation deleteBoard($number: Int) {
+    deleteBoard(number: $number) {
+      message
+    }
+  }
+`;
+
+export default function StaticRoutingMovedPage() {
+  const { data } = useQuery(FETCH_BOARDS);
+
+  const [deleteBoard] = useMutation(DELETE_BOARD);
+
+  console.log(data?.fetchBoards);
+
+  const onClickDelete = (event) => {
+    deleteBoard({
+      variables: { number: Number(event.target.id) },
+      refetchQueries: [{ query: FETCH_BOARDS }],
+    });
+  };
+
+  return (
+    <div>
+      {data?.fetchBoards.map((el) => (
+        <div key={el.number}>
+          <span>
+            <input type="checkbox" />
+          </span>
+          <span style={{ margin: "10px" }}>{el.number}</span>
+          <span style={{ margin: "10px" }}>{el.title}</span>
+          <span style={{ margin: "10px" }}>{el.writer}</span>
+          <span>
+            <button id={el.number} onClick={onClickDelete}>
+              삭제
+            </button>
+          </span>
+        </div>
+      ))}
+    </div>
+  );
+}
