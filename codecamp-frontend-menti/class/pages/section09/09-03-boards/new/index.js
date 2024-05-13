dd12a61e22f4c056038cd978575d54a4589d66ce
updaun <updauney@daum.net>
2024-05-13 23:19:20 +0900

    feat: 09-03-boards 수정 1편
    - board 수정하기 1차 구현

---
+++ codecamp-frontend-menti/class/pages/section09/09-03-boards/new/index.js
@@ -0,0 +1,10 @@
+import BoardWrite from "../../../../src/components/units/board/09-write/BoardWrite.container";
+
+export default function GraphqlMutationInputPage() {
+  return (
+    <div>
+      <div>#### 여기는 페이지 입니다.</div>
+      <BoardWrite isEdit={false} />;<div>#### 여기는 페이지 입니다.</div>
+    </div>
+  );
+}
