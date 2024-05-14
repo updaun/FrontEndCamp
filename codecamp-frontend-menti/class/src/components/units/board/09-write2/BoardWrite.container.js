715b49bf754554a37cc0cc51ebad6a56f7d034e8
updaun <updauney@daum.net>
2024-05-14 23:29:13 +0900

    feat: 09-04-boards 수정 2편
    - 기본값 넣기
    - 수정한 것만 백엔드 보내기

---
+++ codecamp-frontend-menti/class/src/components/units/board/09-write2/BoardWrite.container.js
@@ -0,0 +1,67 @@
+import { useMutation } from "@apollo/client";
+import { useState } from "react";
+import BoardWriteUI from "./BoardWrite.presenter";
+import { 나의그래프큐엘셋팅, UPDATE_BOARD } from "./BoardWrite.queries";
+import { useRouter } from "next/router";
+
+export default function BoardWrite(props) {
+  const router = useRouter();
+  const [writer, setWriter] = useState("");
+  const [title, setTitle] = useState("");
+  const [contents, setContents] = useState("");
+
+  const [나의함수] = useMutation(나의그래프큐엘셋팅);
+  const [updateBoard] = useMutation(UPDATE_BOARD);
+
+  const onClickSubmit = async () => {
+    const result = await 나의함수({
+      // variables 이게 $ 역할을 함
+      variables: {
+        writer: writer,
+        title: title,
+        contents: contents,
+      },
+    });
+    console.log(result);
+    router.push(`/section09/09-04-boards/${result.data.createBoard.number}`);
+  };
+
+  const onClickUpdate = async () => {
+    const myvariables = {
+      number: Number(router.query.number),
+    };
+    if (writer) myvariables.writer = writer;
+    if (title) myvariables.title = title;
+    if (contents) myvariables.contents = contents;
+    const result = await updateBoard({
+      variables: {
+        ...myvariables,
+      },
+    });
+    router.push(`/section09/09-04-boards/${result.data.updateBoard.number}`);
+  };
+
+  const onChangeWriter = (event) => {
+    setWriter(event.target.value);
+  };
+
+  const onChangeTitle = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const onChangeContents = (event) => {
+    setContents(event.target.value);
+  };
+
+  return (
+    <BoardWriteUI
+      onChangeWriter={onChangeWriter}
+      onClickUpdate={onClickUpdate}
+      onChangeTitle={onChangeTitle}
+      onChangeContents={onChangeContents}
+      onClickSubmit={onClickSubmit}
+      isEdit={props.isEdit}
+      data={props.data} //undefined 이거나, 데이터가 있을 수 있음
+    />
+  );
+}
