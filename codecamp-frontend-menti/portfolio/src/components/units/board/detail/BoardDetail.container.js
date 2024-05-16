a9b12ec0c4535e246379522cc4bc61afca125e58
updaun <updauney@daum.net>
2024-05-16 20:25:46 +0900

    feat: react portfolio review 9-1 update

--- codecamp-frontend-menti/portfolio/src/components/units/board/detail/BoardDetail.container.js
+++ codecamp-frontend-menti/portfolio/src/components/units/board/detail/BoardDetail.container.js
@@ -1,14 +1,23 @@
 import { useQuery } from "@apollo/client";
 import { useRouter } from "next/router";
 import BoardDetailUI from "./BoardDetail.presenter";
-import { FETCH_BOARD } from './BoardDetail.queries'
+import { FETCH_BOARD } from "./BoardDetail.queries";
 
-export default function BoardDetail(){
-    const router = useRouter()
+export default function BoardDetail() {
+  const router = useRouter();
 
-    const { data } = useQuery(FETCH_BOARD, {
-      variables: { boardId: router.query.boardId },
-    });
-    
-    return <BoardDetailUI data={data} />
+  const { data } = useQuery(FETCH_BOARD, {
+    variables: { boardId: router.query.boardId },
+  });
+
+  const onClickMoveToBoardEdit = () => {
+    router.push(`/boards/${router.query.boardId}/edit`);
+  };
+
+  return (
+    <BoardDetailUI
+      data={data}
+      onClickMoveToBoardEdit={onClickMoveToBoardEdit}
+    />
+  );
 }
